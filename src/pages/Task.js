1752962b8d45650397a8ec7fefdd6e6fe7aaf3e0
Akidainu <769902400@qq.com>
2018-10-10 17:44:38 +0800

    TaskTab

--- src/pages/Task.js
+++ src/pages/Task.js
@@ -1,13 +1,62 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {createMaterialTopTabNavigator} from 'react-navigation';
+
+class ScheduleListScreen extends Component<Props> {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>Schedule page!</Text>
+            </View>
+        );
+    }
+}
+
+class TaskListScreen extends Component<Props> {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>TaskList page!</Text>
+            </View>
+        );
+    }
+}
+
+class MemoListScreen extends Component<Props> {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>MemoList page!</Text>
+            </View>
+        );
+    }
+}
+
+class RecycledListScreen extends Component<Props> {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>RecycledList page!</Text>
+            </View>
+        );
+    }
+}
+
+const TaskTab = createMaterialTopTabNavigator({
+    ScheduleList:{screen: ScheduleListScreen,navigationOptions: {tabBarLabel:'日程'}},
+    TaskList:{screen: TaskListScreen,navigationOptions: {tabBarLabel:'任务'}},
+    MemoList:{screen: MemoListScreen,navigationOptions: {tabBarLabel:'便签'}},
+    RecycledList:{screen: RecycledListScreen,navigationOptions: {tabBarLabel:'回收站'}},
+},{
+    navigationOptions:{
+    }
+});
 
 type Props = {};
 export default class Task extends Component<Props> {
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>Task page!</Text>
-            </View>
+            <TaskTab />
         );
     }
 }
